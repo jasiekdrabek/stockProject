aa70d5bdf899e6a041820dde5fee53972a9c7c25
Jan Drabek <33521261+jasiekdrabek@users.noreply.github.com>
2024-07-16 19:21:32 +0200

    buy offer update

--- stockApp/serializers/buyOfferSerializer.py
+++ stockApp/serializers/buyOfferSerializer.py
@@ -6,3 +6,7 @@ class BuyOfferSerializer(serializers.ModelSerializer):
         model = BuyOffer
         fields = '__all__'
         read_only_fields = ['user']
+
+    def create(self, validated_data):
+        user = validated_data.pop('user')
+        return BuyOffer.objects.create(user=user, **validated_data)
--- stockApp/views/buyOffer.py
+++ stockApp/views/buyOffer.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from stockApp.serializers import BuyOfferSerializer
+from stockApp.models import BuyOffer
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
@@ -14,3 +15,21 @@ def addBuyOffer(request):
         serializer.save(user=request.user)  # Przekazanie użytkownika do metody save serializera
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)  # Zwracanie błędów walidacji
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def buyOffers(request):
+    buy_offers = BuyOffer.objects.filter(user=request.user)
+    serializer = BuyOfferSerializer(buy_offers, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def deleteBuyOffer(request,pk):
+    try:
+        buy_offer = BuyOffer.objects.get(pk=pk, user=request.user)
+    except BuyOffer.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    buy_offer.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
