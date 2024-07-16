44f0c73969f7e379e8eebc4551d1a1037ee44bf4
Jan Drabek <33521261+jasiekdrabek@users.noreply.github.com>
2024-07-16 19:22:01 +0200

    add update user data

---
+++ stockApp/serializers/updateUserSerializer.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from stockApp.models import CustomUser
+
+class UserUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['name', 'surname', 'money', 'role']
+    
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.surname = validated_data.get('surname', instance.surname)
+        instance.money += validated_data.get('money', instance.money)
+        instance.role = validated_data.get('role', instance.role)
+        instance.save()
+        return instance
---
+++ stockApp/views/user.py
@@ -0,0 +1,17 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from stockApp.serializers import UserUpdateSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def addMoney(request):
+    user = request.user
+    print(user)
+    print(request.data)
+    serializer = UserUpdateSerializer(user, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
