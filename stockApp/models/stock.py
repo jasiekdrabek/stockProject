94c175c501cfb1811a58eab0351bcaf23cd9fb76
Jan Drabek <33521261+jasiekdrabek@users.noreply.github.com>
2024-07-30 23:23:51 +0200

    Update stock.py

--- stockApp/models/stock.py
+++ stockApp/models/stock.py
@@ -3,6 +3,6 @@ from .company import Company
 from .user import CustomUser
 
 class Stock(models.Model):
-    money = models.IntegerField(default=0)
+    amount = models.IntegerField(default=0)
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
