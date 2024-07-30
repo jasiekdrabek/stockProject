4b4e070489241a9e004c0c2b80806b9fd9b7fcfd
Jan Drabek <33521261+jasiekdrabek@users.noreply.github.com>
2024-07-30 23:24:21 +0200

    Update sellOfferSerializer.py

--- stockApp/serializers/sellOfferSerializer.py
+++ stockApp/serializers/sellOfferSerializer.py
@@ -1,12 +1,30 @@
 from rest_framework import serializers
-from stockApp.models import SellOffer
+from rest_framework import serializers
+from stockApp.models import SellOffer, Stock, Company
 
 class SellOfferSerializer(serializers.ModelSerializer):
     class Meta:
         model = SellOffer
-        fields = '__all__'
-        read_only_fields = ['user']
+        fields = ['company', 'minPrice', 'startAmount', 'amount', 'dateLimit', 'actual']
+        # Usuwamy pole 'user' i 'stock', bo będą dodawane automatycznie
 
     def create(self, validated_data):
-        user = validated_data.pop('user')
-        return SellOffer.objects.create(user=user, **validated_data)
+        request = self.context.get('request')
+        company = validated_data['company']
+        amount = validated_data['amount']
+
+        # Sprawdzanie, czy użytkownik ma wystarczającą ilość akcji
+        try:
+            stock = Stock.objects.get(user=request.user, company=company)
+            if stock.amount < amount:
+                raise serializers.ValidationError("You do not have enough shares.")
+        except Stock.DoesNotExist:
+            raise serializers.ValidationError("You do not own shares of this company.")
+
+        # Tworzenie oferty sprzedaży i aktualizacja ilości akcji
+        sell_offer = SellOffer.objects.create(
+            user=request.user,
+            stock=stock,
+            **validated_data
+        )
+        return sell_offer
