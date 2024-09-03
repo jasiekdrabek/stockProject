b47b00d5c990db4164832082b1a24b0c2ef4f8f5
Jan Drabek <jasiekdrabek@gmail.com>
2024-09-03 17:12:46 +0200

    change if error

--- stockProject/stockApp/middleware/LogRequestMiddleware.py
+++ stockProject/stockApp/middleware/LogRequestMiddleware.py
@@ -1,4 +1,5 @@
 # stockApp/middleware/LogRequestMiddleware.py
+import datetime
 import time
 from django.utils.deprecation import MiddlewareMixin
 from django.db import connection
@@ -6,7 +7,7 @@ from stockApp.models import MarketLog
 
 class LogRequestMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        # Store start time and generate request ID
+        # Store start time
         request.start_time = time.time()
 
     def process_response(self, request, response):
@@ -17,17 +18,20 @@ class LogRequestMiddleware(MiddlewareMixin):
         if hasattr(request, 'start_time') and hasattr(response, 'data'):
             total_time = time.time() - request.start_time
             db_time = sum(float(query['time']) for query in connection.queries)
-            if isinstance(response.data, list):
+            id = None
+            if isinstance(response.data, list) and 'request_id' in response.data[-1]:
                 id = response.data[-1]["request_id"]
-            else:
+            if isinstance(response.data, dict) and 'request_id' in response.data:
                 id = response.data['request_id']
             # Log the request
-            MarketLog.objects.using('test').create(
-                api_method=request.method,
-                application_time=total_time,
-                database_time=db_time,
-                endpoint_url=request.get_full_path(),
-                request_id=id
-            )
+            if id:
+                MarketLog.objects.using('test').create(
+                    api_method=request.method,
+                    application_time=total_time,
+                    database_time=db_time,
+                    endpoint_url=request.get_full_path(),
+                    request_id=id,
+                    timestamp = datetime.datetime.now()
+                )
 
         return response
