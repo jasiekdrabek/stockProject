697c1d89df64dbbffe652015bab695499d54df89
Jan Drabek <jasiekdrabek@gmail.com>
2024-09-18 12:53:57 +0200

    load testing code refactor

--- loadTestingApp/locustfile.py
+++ loadTestingApp/locustfile.py
@@ -8,13 +8,13 @@ from datetime import datetime
 import os
 from gevent.lock import Semaphore
 
-time=float(os.getenv("TIME_BETWEEN_REQUESTS"))
-all_locusts_spawned = Semaphore()
-all_locusts_spawned.acquire()
+timeBetweenRequests=float(os.getenv("TIME_BETWEEN_REQUESTS"))
+allLocustsSpawned = Semaphore()
+allLocustsSpawned.acquire()
 
 @events.spawning_complete.add_listener
-def on_hatch_complete(**kw):
-    all_locusts_spawned.release()
+def onHatchComplete(**kw):
+    allLocustsSpawned.release()
 
 
 # Konfiguracja połączenia z bazą danych
@@ -30,12 +30,12 @@ cursor = conn.cursor()
 # Inicjalizujemy generator danych
 fake = faker.Faker()
 
-def generate_random_data():
+def generateRandomData():
     username = fake.user_name()
     name = fake.first_name()
     surname = fake.last_name()
     email = fake.email()
-    password = generate_valid_password()
+    password = generateValidPassword()
 
     return {
         "username": username,
@@ -45,27 +45,69 @@ def generate_random_data():
         "email": email
     }
 
-def generate_valid_password():
+def generateValidPassword():
     password = ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation, k=12))
     return password
 
+def register(self):
+    login = generateRandomData()
+    self.client.post("/api/signUp", json=login)
+    data = {
+        'username': login['username'],
+        'password': login['password']
+    }
+    response = self.client.post("/api/signIn",json=data)
+    self.token = response.json()['token']
+    companyName = fake.company()
+    self.client.post("/api/addCompany",headers={"authorization": "Token " + self.token}, json={"name":companyName})
+    allLocustsSpawned.wait()    
+
+def buyOffer(self, companyId, maxAmount):
+    userInfoResponse = self.client.get("/api/user", headers={"authorization": "Token " + self.token})
+    userInfo = userInfoResponse.json()
+    if userInfo['moneyAfterTransations'] < 1000:
+        return 0
+    amount = random.randint(1, maxAmount)
+    buyOfferData = {
+        "company": companyId,
+        "startAmount": amount,
+        "amount": amount,
+    }
+    self.client.post("/api/addBuyOffer", headers={"authorization": "Token " + self.token}, json=buyOfferData)
+    return 1
+    
+def sellOffer(self, maxAmount,companyId = None):
+    response = self.client.get("/api/user/stocks", headers={"authorization": "Token " + self.token})
+    userStocks = response.json()
+    userStocks.pop()
+    if not userStocks:
+        return 0
+    if companyId == None:
+        stock = random.choice(userStocks)
+        companyId = stock['company']
+    else:
+        stock = next((s for s in userStocks if s['company'] == companyId), None)
+        if stock is None:
+            return 0
+    availableAmount = stock['amount']
+    if availableAmount == 0:
+        return 0
+    amountToSell = random.randint(1, min(availableAmount,maxAmount))
+    sellOfferData = {
+        "company": companyId,
+        "startAmount": amountToSell,
+        "amount": amountToSell,
+    }
+    self.client.post("/api/addSellOffer", headers={"authorization": "Token " + self.token}, json=sellOfferData)
+    return 1
+    
 class WebsiteReadOnlyUser(FastHttpUser):
     weight = 1
-    wait_time = constant(time)
+    wait_time = constant(timeBetweenRequests)
     token = ''
     
     def on_start(self):
-        login = generate_random_data()
-        self.client.post("/api/auth/signUp", json=login)
-        data = {
-            'username': login['username'],
-            'password': login['password']
-        }
-        response = self.client.post("/api/auth/signIn",json=data)
-        self.token = response.json()['token']
-        company_name = fake.company()
-        self.client.post("/api/company/addCompany",headers={"authorization": "Token " + self.token}, json={"name":company_name})
-        all_locusts_spawned.wait()
+        register(self)        
 
     @task
     def getSellOffers(self):
@@ -87,24 +129,13 @@ class WebsiteReadOnlyUser(FastHttpUser):
     def getCompanies(self):
         self.client.get("/api/companies",headers={"authorization": "Token " + self.token})
         
-class WebsiteActiveUser(HttpUser):
+class WebsiteActiveUser(FastHttpUser):
     weight = 2
-    #wait_time = between(0.2, 0.5)
-    wait_time = constant(time)
+    wait_time = constant(timeBetweenRequests)
     token = ''
     
     def on_start(self):
-        login = generate_random_data()
-        self.client.post("/api/signUp", json=login)
-        data = {
-            'username': login['username'],
-            'password': login['password']
-        }
-        response = self.client.post("/api/signIn",json=data)
-        self.token = response.json()['token']
-        company_name = fake.company()
-        self.client.post("/api/addCompany",headers={"authorization": "Token " + self.token}, json={"name":company_name})
-        all_locusts_spawned.wait()
+        register(self)
 
     @task
     def wait(self):
@@ -112,56 +143,76 @@ class WebsiteActiveUser(HttpUser):
     
     @task
     def addSellOffer(self):
-        # Sprawdzenie, w jakich firmach użytkownik ma akcje
-        response = self.client.get("/api/user/stocks", headers={"authorization": "Token " + self.token})
-        user_stocks = response.json()
-        if len(user_stocks) > 1:
-            # Losowanie firmy, z której sprzedamy akcje
-            stocks_to_choose_from = user_stocks[:-1]
-            selected_stock = random.choice(stocks_to_choose_from)
-            company_id = selected_stock['company']
-            available_amount = selected_stock['amount']
-            if available_amount == 0:
-                return
-            # Losowanie liczby akcji do sprzedaży (minimum 1, maksimum dostępne akcje)
-            amount_to_sell = random.randint(1, min(available_amount,10))
-
-            # Dodanie oferty sprzedaży
-            sell_offer_data = {
-                "company": company_id,
-                "startAmount": amount_to_sell,
-                "amount": amount_to_sell,
-            }
-
-            self.client.post("/api/addSellOffer", headers={"authorization": "Token " + self.token}, json=sell_offer_data)
+        sellOffer(self, 10)
 
     @task
     def addBuyOffer(self):
-        # 1. Pobierz dane użytkownika, aby sprawdzić stan konta
-        user_info_response = self.client.get("/api/user", headers={"authorization": "Token " + self.token})
-        user_info = user_info_response.json()
-        # 2. Sprawdź, czy użytkownik ma co najmniej 1000 jednostek waluty
-        if user_info['moneyAfterTransations'] >= 1000:
-            # 3. Pobierz listę firm dostępnych w systemie
-            response = self.client.get("/api/companies", headers={"authorization": "Token " + self.token})
-            companies = response.json()
-            
-            # 4. Wybierz losową firmę
-            if len(companies) > 1:
-                companies = companies[:-1]
-                company = random.choice(companies)
-                company_id = company['id']
+        response = self.client.get("/api/companies", headers={"authorization": "Token " + self.token})
+        companies = response.json()    
+        companies.pop()        
+        if not companies:
+            return
+        company = random.choice(companies)
+        companyId = company['id']
+        buyOffer(self, companyId, 10)
                 
-                # 3. Określ losową liczbę akcji do zakupu i maksymalną cenę, jaką użytkownik jest gotów zapłacić
-                amount = random.randint(1, 10)  # Losowa liczba akcji do zakupu
-                
-                # 4. Wyślij ofertę kupna
-                buy_offer_data = {
-                    "company": company_id,
-                    "startAmount": amount,
-                    "amount": amount,
-                }
-                self.client.post("/api/addBuyOffer", headers={"authorization": "Token " + self.token}, json=buy_offer_data)
+class WebsiteActiveUserWtihMarketAnalize(FastHttpUser):
+    weight = 2
+    numberOfRates = 3
+    wait_time = constant(timeBetweenRequests)
+    token = ''
+    limitNumerOfBuyOffersInOneTask = 2
+    limitNumerOfSellOffersInOneTask = 3
+    
+    def on_start(self):
+        register(self)
+
+    @task
+    def wait(self):
+        pass
+    
+    @task
+    def checkOffers(self):
+        numberOfBuyOffers = 0
+        numberOfSellOffers = 0
+        response = self.client.get("/api/getCompaniesStockRates", headers={"authorization": "Token " + self.token}, json={'numberOfRates' : self.numberOfRates})
+        try:
+            stockRates = response.json()
+        except(ValueError, KeyError):
+            return
+        stockRates.pop()
+        if not stockRates:
+            return
+        companiesStockRates = {}
+        for rate in stockRates:
+            companyId = rate['company']
+            price = rate['rate']
+            if companyId not in companiesStockRates:
+                companiesStockRates[companyId] = []
+            companiesStockRates[companyId].append(price)
+        shuffledCompanies = list(companiesStockRates.keys())
+        random.shuffle(shuffledCompanies)
+        for companyId in shuffledCompanies:
+            priceHistories = companiesStockRates[companyId]
+            if numberOfBuyOffers >= self.limitNumerOfBuyOffersInOneTask and numberOfSellOffers >= self.limitNumerOfSellOffersInOneTask:
+                return
+            if len(priceHistories) < self.numberOfRates:
+                continue
+            priceTrend = self.get_priceTrend(list(reversed(priceHistories)))
+            if priceTrend == "nierosnący" and numberOfBuyOffers < self.limitNumerOfBuyOffersInOneTask:
+                numberOfBuyOffers += buyOffer(self, companyId, 3)            
+            if priceTrend == "niemalejący" and numberOfSellOffers < self.limitNumerOfSellOffersInOneTask:
+                numberOfSellOffers += sellOffer(self, 3, companyId=companyId)
+
+    def get_priceTrend(self, priceHistory):
+        isNiemalejący = all(x <= y for x, y in zip(priceHistory, priceHistory[1:]))
+        isNierosnący = all(x >= y for x, y in zip(priceHistory, priceHistory[1:]))
+        if isNiemalejący:
+            return "niemalejący"
+        elif isNierosnący:
+            return "nierosnący"
+        else:
+            return None
            
     @events.request.add_listener
     def log_request(request_type, name, response_time, response_length, response, context, exception, **kwargs):
@@ -171,15 +222,11 @@ class WebsiteActiveUser(HttpUser):
             else:
                 id = response.json()['request_id']
         except (ValueError, KeyError):
-            # Jeśli nie można uzyskać request_id z odpowiedzi, użyj tymczasowego lub domyślnego
-            id = 'unknown'
-        local_tz = pytz.timezone('UTC')
-        timestamp = datetime.now(local_tz).strftime('%Y-%m-%d %H:%M:%S')
-
-        # Zapisanie danych do modelu TrafficLog
+            return
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         cursor.execute(
             """INSERT INTO "stockApp_trafficlog" (timestamp, request_id, api_time) VALUES (%s, %s, %s)""",
-            (timestamp, id, response_time / 1000.0)  # Konwersja na sekundy
+            (timestamp, id, response_time / 1000.0)
         )
-        conn.commit()  # Zatwierdzenie transakcji
+        conn.commit()
     
--- loadTestingApp/start_locust.sh
+++ loadTestingApp/start_locust.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
-# Opóźnienie przed uruchomieniem Locust
 sleep 60
-
-# Parametry testu
-USERS=${LOCUST_USERS:-30}  # Domyślnie 10 użytkowników, jeśli zmienna nie jest ustawiona
-SPAWN_RATE=${LOCUST_SPAWN_RATE:-1}  # Domyślna wartość dla ramp-up to 1 user/s
-TIME=${LOCUST_TIME:-5m}  # Domyślny host, jeśli zmienna nie jest ustawiona
-LOCUST_CLASS=${LOCUST_CLASS:-WebsiteActiveUser}  # Domyślna klasa użytkownika
-
-# Uruchomienie Locust z podanymi parametrami
+USERS=${LOCUST_USERS:-30}
+SPAWN_RATE=${LOCUST_SPAWN_RATE:-1}
+TIME=${LOCUST_TIME:-5m}
+LOCUST_CLASS=${LOCUST_CLASS:-WebsiteActiveUser}
 locust -f /app/locustfile.py --headless -u $USERS -r $SPAWN_RATE --host=http://web:8080 -t $TIME $LOCUST_CLASS
